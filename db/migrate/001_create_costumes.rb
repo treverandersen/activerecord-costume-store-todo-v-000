8e4297e44f5c15850deff8b80543503128c37ba8
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-17 23:41:56 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class Costume < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :size
       t.string :image_url
     end

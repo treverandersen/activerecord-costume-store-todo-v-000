2d8f838a91fd265265d43695e0796cd09522ba52
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-18 01:00:50 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening
-      t.datetime :closing
+      t.string :opening
+      t.string :closing
     end
   end
 end

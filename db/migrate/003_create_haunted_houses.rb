cd30dc763fd52d669490d0b60da61520dccb4b50
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-18 01:03:37 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.text :description
+      t.string :description
     end
   end
 end
